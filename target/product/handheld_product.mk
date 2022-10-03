3ae7a4c5284242b39021a524e6a6af880bf56e42
Michael Bestas <mkbestas@lineageos.org>
2022-10-03 18:13:09 +0300

    Exclude Camera2 for custom builds
    Change-Id: I64daac3c0ba5511dea6fb9abcb204d679fe01bef

--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -24,7 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/media_product.mk)
 PRODUCT_PACKAGES += \
     Browser2 \
     Calendar \
-    Camera2 \
     Contacts \
     DeskClock \
     Gallery2 \
@@ -36,7 +35,8 @@ PRODUCT_PACKAGES += \
 
 ifeq ($(HERTZIFY_BUILD),)
 PRODUCT_PACKAGES += \
-    LatinIME
+    LatinIME \
+    Camera2
 endif
 
 PRODUCT_PACKAGES_DEBUG += \
